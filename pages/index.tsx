42c89406c6b26386fc410826b163c754703009ad
Picu1995 <prashanth.s@vuedata.in>
2023-07-13 15:26:09 +0530

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { Box } from '@bigcommerce/big-design';
-import styled from 'styled-components';
 import ErrorMessage from '../components/error';
 import { useProducts } from '../lib/hooks';
 
@@ -7,11 +5,8 @@ const Index = () => {
     const { error } = useProducts();
 
     if (error) return <ErrorMessage error={error} />;
+
     return <h1>Welcome to Nextjs 12 Sample Program devops</h1>;
 };
-
-const StyledBox = styled(Box)`
-    min-width: 10rem;
-`;
 
 export default Index;
