a202884001952e792518a2b519c1b1ba4e279d10
Picu1995 <prashanth.s@vuedata.in>
2023-07-13 15:12:41 +0530

    build issue fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,13 @@
-import { Box, Flex, H1, H4, Panel } from '@bigcommerce/big-design';
+import { Box } from '@bigcommerce/big-design';
 import styled from 'styled-components';
 import ErrorMessage from '../components/error';
-import Loading from '../components/loading';
 import { useProducts } from '../lib/hooks';
 
 const Index = () => {
-    const { error, isLoading, summary } = useProducts();
+    const { error } = useProducts();
 
-    //if (isLoading) return <Loading />;
     if (error) return <ErrorMessage error={error} />;
-return <h1>Welcome to Nextjs 12 Sample Program devops</h1>
-    return (
-        <Panel header="Homepage" id="home">
-            <Flex>
-                <StyledBox border="box" borderRadius="normal" marginRight="xLarge" padding="medium">
-                    <H4>Inventory count</H4>
-                    <H1 marginBottom="none">{summary.inventory_count}</H1>
-                </StyledBox>
-                <StyledBox border="box" borderRadius="normal" marginRight="xLarge" padding="medium">
-                    <H4>Variant count</H4>
-                    <H1 marginBottom="none">{summary.variant_count}</H1>
-                </StyledBox>
-                <StyledBox border="box" borderRadius="normal" padding="medium">
-                    <H4>Primary category</H4>
-                    <H1 marginBottom="none">{summary.primary_category_name}</H1>
-                </StyledBox>
-            </Flex>
-        </Panel>
-    );
+    return <h1>Welcome to Nextjs 12 Sample Program devops</h1>;
 };
 
 const StyledBox = styled(Box)`
